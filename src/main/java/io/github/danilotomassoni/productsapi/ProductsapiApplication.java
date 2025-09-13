9aaf3cff4003ba4c4d14cc907ca6265d3e90163f
danilotomassoni <daniloaugustotomassoni@gmail.com>
2025-09-13 11:59:15 -0300

    Criando Entidade Produto

--- src/main/java/io/github/danilotomassoni/productsapi/ProductsapiApplication.java
+++ src/main/java/io/github/danilotomassoni/productsapi/ProductsapiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductsapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsapiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsapiApplication.class, args);
+    }
 }
